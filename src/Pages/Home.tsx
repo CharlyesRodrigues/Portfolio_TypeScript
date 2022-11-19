170cf29c0eafae457a609d59370a6613b8e39113
CharlyesRodrigues <charlyessouza@gmail.com>
2022-11-19 20:29:11 -0300

    Atualizado

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,12 @@
+import i18next from 'i18next';
+import { useTranslation } from 'react-i18next';
+
+
+
 
 export function Home() {
 
-
+    const {t} = useTranslation();
     return (
   
      <div> <h1>Teste</h1></div>
