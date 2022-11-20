f17b78a5f7b2df14005a103128f8216a83f21a73
CharlyesRodrigues <charlyessouza@gmail.com>
2022-11-20 13:57:16 -0300

    Atualizado

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,14 +1,30 @@
 
-import {Rectangle_4234,      } from './style';
+import {Rectangle,Lista1,Header} from './style';
 
 
 export function Home() {
 
 
-    return (
-     <Rectangle_4234>
-    <h1> Teste</h1>
-   
-    </Rectangle_4234>
+return (
+
+    <Header>
+     <Rectangle>
+        
+        <h1>Olá, meu nome é Charlyes Souza Rodrigues e eu sou desenvolvedor Front-end, Tecnologias que tenho experiências:</h1>
+        <Lista1>
+        <ul><li>JavaScript</li></ul>
+        <ul><li>React</li></ul>
+        <ul><li>Vue Js</li></ul>
+        <ul><li>Tailwind CSS</li></ul>
+        </Lista1>
+
+        
+
+
+
+    </Rectangle>
+    
+    </Header>
+     
     )
   }
--- src/Pages/Home/style.ts
+++ src/Pages/Home/style.ts
@@ -1,13 +1,92 @@
 import styled from "styled-components";
 
 
-export const Rectangle_4234 = styled.div`
 
-h1{
+export const Header = styled.div`
 
-    color: var(--dany);
-}
 
+position: absolute;
+width: 1440px;
+height: 72px;
+left: 0px;
+top: 0px;
+
+/* Dark/surface-secondary */
+
+background: #27272A;
 
 
 `
+;
+
+
+export const Rectangle = styled.div `
+
+
+h1{
+    position: absolute;
+    width: 791px;
+    height: 76px;
+    left: 325px;
+    top: 10.5rem;
+    
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 160%;
+    /* or 38px */
+    
+    text-align: center;
+    
+    color: #FFFFFF;
+    
+}
+
+box-sizing: border-box;
+
+position: absolute;
+width: 793px;
+height: 173px;
+left: 324px;
+top: 305px;
+
+/* Dark/surface-secondary */
+
+background: #27272A;
+border: 1px solid #9D4B00;
+border-radius: 8px;
+
+
+ 
+
+`;
+export const Lista1= styled.div`
+
+position: absolute;
+width: 157px;
+height: 128px;
+left: 380px;
+top: 327px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 600;
+font-size: 20px;
+line-height: 160%;
+/* or 32px */
+
+
+color: #EDEDED;
+
+`;
+
+
+
+
+
+
+
+
+
+
