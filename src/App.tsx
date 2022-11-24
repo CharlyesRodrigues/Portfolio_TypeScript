16487e4f72f5690a8555617ac32dd71fadd162d0
CharlyesRodrigues <charlyessouza@gmail.com>
2022-11-24 20:12:55 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {Header} from './Header/index'
 import {Home} from './Pages/Home'
-import { Traduzir } from './Translation'
+
 import {GlobalStyle } from './Styles/global'
 import {Botao } from './Components/Botao'
 
