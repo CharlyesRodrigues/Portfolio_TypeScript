934f5ba32a7ec3cdf3d0c5a3cdfbb6770193565d
CharlyesRodrigues <charlyessouza@gmail.com>
2022-11-23 21:47:30 -0300

    Update index.tsx

--- src/Translation/index.tsx
+++ src/Translation/index.tsx
@@ -1,15 +1,14 @@
  import {Texto} from './styles'
- 
+
  
  export function Traduzir() {
 
-
+  
+    
     return (
      
     <>
-  <Texto>    
-    "Hello, my name is Charlyes Souza Rodrigues and I am a Front-end developer, Technologies that I have experiences:"
-  </Texto>
+ <Texto>x</Texto>
     
    
      </>
